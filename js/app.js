7d87914ca1f2f90dc227d185d1cc68b1006afb1a
Dotun Ogunsakin <email@dotun.me>
2018-06-10 11:13:48 -0400

    Implemented map pin filtering using search box.

--- js/app.js
+++ js/app.js
@@ -1,72 +1,71 @@
 function ViewModel() {
-  var map, geocoder, infoWindow, infoPane;
+  var self, map, pins, geocoder, infoWindow, infoPane;
+
+  self = this;
   
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 34.033222, lng: -84.4709025},
     zoom: 12
   });
-
+  pins = [];
   geocoder = new google.maps.Geocoder();
   infoWindow = new google.maps.InfoWindow();
   infoPane = document.getElementById("info-pane");
-  this.searchValue = ko.observable("");
-  
-  // this.filterMarkers = ko.computed(function() {
-  //   for(var marker of markers) {
-  //     if(marker.title.toLowerCase().includes(this.searchValue().toLowerCase())) {
-  //       marker.setVisible(true);
-  //     }
-  //     else {
-  //       marker.setVisible(false);
-  //     }
-  //   }
-  // });
+  self.query = ko.observable("");
 
   for(var marker of markers) {
     var markerInstance = new google.maps.Marker({
       map: map,
-      position: {lat: marker.coords.lat, lng: marker.coords.lng}
+      title: marker.title,
+      position: {lat: marker.coords.lat, lng: marker.coords.lng},
+      animation: google.maps.Animation.DROP,
     });
 
-    google.maps.event.addListener(markerInstance, "mouseover", (function(marker) {
-      return function() {
-      infoWindow.setContent(marker.title);
+    pins.push(markerInstance);
+
+    google.maps.event.addListener(markerInstance, "mouseover", function() {
+      infoWindow.setContent(this.title);
       infoWindow.open(map, this);
-      }
-    })(marker));
+    });
 
-    google.maps.event.addListener(markerInstance, "mouseout", (function() {
-      return function() {
+    google.maps.event.addListener(markerInstance, "mouseout", function() {
       infoWindow.close();
-      }
-    })(marker));
+    });
 
-    google.maps.event.addListener(markerInstance, "click", (function(marker) {
-      return function() {
-        var infoPaneContent = infoPane.getElementsByClassName("info-pane-content")[0];
-        var latLng = new google.maps.LatLng(marker.coords.lat, marker.coords.lng);
-        
-        // Get address from marker's lat-lng
-        geocoder.geocode({location: latLng}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            if(results[0]) {
-              infoPaneContent.innerHTML = `
-                                            <h3 class="location-name">${marker.title}</h3>
-                                            <div class="location-address">${results[0].formatted_address}</div>
-                                          `;
-              infoPane.classList.add("open");
-            }
+    google.maps.event.addListener(markerInstance, "click", function() {
+      var infoPaneContent = infoPane.getElementsByClassName("info-pane-content")[0];
+      var latLng = new google.maps.LatLng(this.position.lat(), this.position.lng());
+      var closureTitle = this.title;
+      
+      // Get address from pin's lat-lng
+      geocoder.geocode({location: latLng}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if(results[0]) {
+            infoPaneContent.innerHTML = `
+                                          <h3 class="location-name">${closureTitle}</h3>
+                                          <div class="location-address">${results[0].formatted_address}</div>
+                                        `;
+            infoPane.classList.add("open");
           }
-        });
-      }
-    })(marker));
+        }
+      });
+    });
 
     infoPane.getElementsByClassName("close")[0].addEventListener("click", () => {
       infoPane.classList.remove("open");
     });
-
-    this.obMarkers = ko.observableArray(markers);
   }
+
+  self.filteredPins = ko.computed(function() {
+    for(var pin of pins) {
+      if(pin.title.toLowerCase().includes(self.query().trim().toLowerCase())) {
+        pin.setVisible(true);
+      }
+      else {
+        pin.setVisible(false);
+      }
+    }
+  });
 
   console.log("🎉 Map initialized. 🎉");
 }
