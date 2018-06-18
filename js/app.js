0ece06b43bd56151b26a709f8f55f2345b493dde
Dotun Ogunsakin <email@dotun.me>
2018-06-17 22:19:48 -0400

    Made changes based on feedback v1.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function ViewModel() {
   var self, map, geocoder, infoWindow, locationsList, wikiUrl, infoPane;
 
@@ -14,84 +16,120 @@ function ViewModel() {
   locationsList = document.getElementById("locations-list");
   wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&origin=*";
   self.query = ko.observable("");
+  self.infoPaneText = ko.observable("");
 
   // map pins setup - instantiation, event listeners, Wikipedia link
   for(var marker of markers) {
-    var markerInstance = new google.maps.Marker({
-      map: map,
-      position: {lat: marker.coords.lat, lng: marker.coords.lng},
-      animation: google.maps.Animation.DROP,
-    });
+    var markerInstance = createMapPin(marker, map);
     
-    // This needed to be set outside the instantiation
+    // This needs to be set outside the instantiation
     // above in order to work properly
-    markerInstance.title = ko.observable(marker.title);
+    if(markerInstance) {
+      markerInstance.title = marker.title;
+    }
 
     self.pins.push(markerInstance);
 
-    // Open Google Maps' infoWindow
-    google.maps.event.addListener(markerInstance, "mouseover", function() {
-      infoWindow.setContent(this.title());
-      infoWindow.open(map, this);
-    });
+    google.maps.event.addListener(markerInstance, "mouseover", openInfoWindow);
+    google.maps.event.addListener(markerInstance, "mouseout", closeInfoWindow);
+    google.maps.event.addListener(markerInstance, "click", loadAndOpenInfoPane);
+    infoPane.getElementsByClassName("close")[0].addEventListener("click", closeInfoPane);
+  }
 
-    // close Google Maps' infoWindow
-    google.maps.event.addListener(markerInstance, "mouseout", function() {
-      infoWindow.close();
-    });
-
-    // pins load and open infoPane
-    google.maps.event.addListener(markerInstance, "click", function() {
-      var infoPaneContent = infoPane.getElementsByClassName("info-pane-content")[0];
-      infoPaneContent.innerHTML = "";
-
-      var latLng = new google.maps.LatLng(this.position.lat(), this.position.lng());
-      var closureTitle = this.title();
-
-      // bounce pin
-      this.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout((function() {
-          this.setAnimation(null);
-        }).bind(this), 1000);
-      
-      // get address from pin's lat-lng
-      geocoder.geocode({location: latLng}, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-          if(results[0]) {
-            infoPaneContent.innerHTML = `
-                                          <h3 class="location-name">${closureTitle}</h3>
-                                          <div class="location-address">${results[0].formatted_address}</div>
-                                        `;
-            // get Wikipedia link
-            fetch(wikiUrl + "&titles=" + closureTitle)
-              .then(function(response) {
-              return response.json();
-              })
-              .then(function(data) {
-                if(data.query.pages[0].pageid) {
-                  infoPaneContent.innerHTML += `<div class="wiki-link"><a href="https://en.wikipedia.org/?curid=${data.query.pages[0].pageid}" target="_blank">Wikipedia</a></div>`;
-                }
-              })
-              .catch(function(error) {
-                console.log(error);
-              });
-
-            infoPane.classList.add("open");
-          }
-        }
+  // Create map pin
+  function createMapPin(pinDataSource, targetMap) {
+    if("coords" in pinDataSource && "gm_accessors_" in map) {
+      return new google.maps.Marker({
+        map: targetMap,
+        position: {lat: pinDataSource.coords.lat, lng: pinDataSource.coords.lng},
+        animation: google.maps.Animation.DROP,
       });
-    });
+    }
 
-    // close infoPane
-    infoPane.getElementsByClassName("close")[0].addEventListener("click", () => {
-      infoPane.classList.remove("open");
+    // Error handling if argument(s) is/are invalid
+    alert("There was a problem loading the map pins.")
+  }
+
+  // Open Google Maps' infoWindow
+  function openInfoWindow() {
+    infoWindow.setContent(this.title);
+    infoWindow.open(map, this);
+  }
+
+  // close Google Maps' infoWindow
+  function closeInfoWindow() {
+    infoWindow.close();
+  }
+
+  // load and open infoPane
+  function loadAndOpenInfoPane() {
+    var infoPaneContent = infoPane.getElementsByClassName("info-pane-content")[0];
+
+    var latLng = new google.maps.LatLng(this.position.lat(), this.position.lng());
+    var closureTitle = this.title;
+
+    // bounce pin
+    this.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout((function() {
+        this.setAnimation(null);
+      }).bind(this), 1000);
+    
+    // get address from pin's lat-lng
+    geocoder.geocode({location: latLng}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if(results[0]) {
+          // get Wikipedia link
+          fetch(wikiUrl + "&titles=" + closureTitle)
+            .then(function(response) {
+            return response.json();
+            })
+            .then(function(data) {
+              // we got a valid Wikipedia ID
+              if(data.query.pages[0].pageid) {
+                self.infoPaneText(`
+                                  <h3 class="location-name">${closureTitle}</h3>
+                                  <div class="location-address">${results[0].formatted_address}</div>
+                                  <div class="wiki-link"><a href="https://en.wikipedia.org/?curid=${data.query.pages[0].pageid}" target="_blank">Wikipedia</a></div>
+                                `);
+              }
+              // we didn't get a valid Wikipedia ID
+              else {
+                self.infoPaneText(`
+                                  <h3 class="location-name">${closureTitle}</h3>
+                                  <div class="location-address">${results[0].formatted_address}</div>
+                                  <div class="wiki-link">A Wikipedia link was not found for this location</div>
+                                `);
+              }
+            })
+            .catch(function(error) {
+              // the request itself failed e.g. network error
+              self.infoPaneText(`
+                                <h3 class="location-name">${closureTitle}</h3>
+                                <div class="location-address">${results[0].formatted_address}</div>
+                                <div class="wiki-link">Unable to fetch Wikipedia link</div>
+                              `);
+            });
+
+          infoPane.classList.add("open");
+        }
+      }
     });
-  } // end pins instantiation
+  }
+
+  // populate infoPane
+  self.populateInfoPane = ko.computed(function() {
+    return self.infoPaneText();
+  });
+
+  // close infoPane
+  function closeInfoPane() {
+    infoPane.classList.remove("open");
+  }
 
   // trigger pin when matching list item is clicked
-  triggerClickOnPin = function(data, event) {
+  self.triggerClickOnPin = function(data, event) {
     var matchingPin = self.pins().filter(function(pin) {
-      return pin.title().toLowerCase() === event.target.innerHTML.toLowerCase();
+      return pin.title.toLowerCase() === data.title.toLowerCase();
     })[0];
 
     if(matchingPin) {
@@ -110,7 +148,7 @@ function ViewModel() {
     }
 
     return self.pins().filter(function(pin) {
-      return pin.title()
+      return pin.title
       .toLowerCase()
       .includes(self.query()
                     .trim()
@@ -122,7 +160,7 @@ function ViewModel() {
   // filter pins
   function filterPins() {
     for(var pin of self.pins()) {
-      if(pin.title().toLowerCase().includes(self.query().trim().toLowerCase())) {
+      if(pin.title.toLowerCase().includes(self.query().trim().toLowerCase())) {
         pin.setVisible(true);
       }
       else {
@@ -135,7 +173,7 @@ function ViewModel() {
 }
 
 function mapError() {
-  document.getElementById("map").innerText = "Map could not be initialized.";
+  window.alert("Map could not be initialized.");
 }
 
 function initMap() {
